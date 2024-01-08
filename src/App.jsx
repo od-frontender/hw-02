e2cac1e0b0cc0b8aa2e36dc2af965c6bd6bc4f40
od-frontender <od.frontender@gmail.com>
2024-01-09 01:08:24 +0200

    1

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
-import Table from "./components/Table";
+import Table from './components/Table';
 function App() {
-  return <Table />;
+  return (
+    <>
+      <Table />
+      <h1>Hello</h1>
+    </>
+  );
 }
-
-export default App;
+export default App; 
